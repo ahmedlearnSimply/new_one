96886e9af0974dc106199593b6a58dfcbbd357a1
mrrobot5-a <ahmedadel123422@gmail.com>
2025-05-18 03:40:26 +0300

    home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,12 +12,76 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CircleAvatar(),
-          Text("fasdfasdf"),
-          Text("fasdf", style: TextStyle(fontSize: 24)),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                bottom: 10,
+                left: 10,
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xFFEDF7FA),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: ClipOval(
+                child: Image.asset(
+                  "assets/profile.png",
+                  fit: BoxFit.cover,
+                  width: 170.859375,
+                  height: 170.859375,
+                ),
+              ),
+            ),
+            Gap(34),
+            Text(
+              "Hi, I am John,\nCreative\nTechnologist",
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 32,
+                fontFamily: "hebbo",
+                color: Color(0xFF21243D),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text(
+                "Amet minim mollit non deserunt ullamco est\n"
+                "sit aliqua dolor do amet sint. Velit officia\n"
+                "consequat duis enim velit mollit. Exercitation\n"
+                "veniam consequat sunt nostrud amet.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  fontFamily: "hebbo",
+                  color: Color(0xFF21243D),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(2),
+                color: Color(0xFFFF6464),
+              ),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Download Resume",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    fontFamily: "hebbo",
+                    color: Color(0xFFFFFFFF),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
